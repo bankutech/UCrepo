36135a7ec55fc70c6c9ce3aa36f0cc3b5aad1968
bankutech <sagnik1302@gmail.com>
2026-03-11 09:09:24 +0530

    Application Entry & Welcome Message

---
+++ .idea/UseCase1HostelBookingApp.java
@@ -0,0 +1,7 @@
+public class UseCase1HotelBookingApp {
+        public static void main(String[] args) {
+            System.out.println("Welcome to Book My Stay!");
+            System.out.println("Hotel Booking Management System v1.0");
+            System.out.println("Application started successfully...");
+        }
+    }
